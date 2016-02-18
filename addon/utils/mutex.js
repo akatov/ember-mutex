defde1dae392b86943249d0fb6c6bfeb08f7f77e
Dmitri Akatov <akatov@gmail.com>
2016-02-18 21:03:45 +0000

    imported mutex code

--- addon/utils/mutex.js
+++ addon/utils/mutex.js
@@ -1,3 +1,84 @@
-export default function mutex() {
-  return true;
-}
+import Ember from 'ember';
+
+const {
+  RSVP: {
+    Promise
+  },
+  run,
+  computed: {
+    reads
+  }
+} = Ember;
+
+/**
+ * @module utils
+ */
+
+/**
+ * This is inspired by https://github.com/plenluno/promise-mutex but makes
+ * use of the Ember runloop.
+ * Usage: `Mutex.create().lock(function usesChainedPromises() { ... })`,
+ * where the function `usesChainedPromises` uses chained promises.
+ *
+ * @class Mutex
+ */
+export default Ember.Object.extend({
+
+  _locked: false,
+
+  _lock() {
+    if (this.get('_locked')) {
+      return false;
+    } else {
+      this.set('_locked', true);
+      return true;
+    }
+  },
+
+  _unlock() {
+    if (this.get('_locked')) {
+      this.set('_locked', false);
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  lock(f) {
+    let executor = run.bind(this, function(resolve, reject) {
+      if (!this._lock()) {
+        run.next(null, executor, resolve, reject);
+        return;
+      }
+
+      if (!(f instanceof Function)) {
+        reject(new Error('argument not function'));
+        this._unlock();
+        return;
+      }
+
+      var r;
+
+      try {
+        r = f();
+      } catch (e) {
+        reject(e);
+        this._unlock();
+        return;
+      }
+
+      Promise.resolve(r).then((res) => {
+        resolve(res);
+        this._unlock();
+      }).catch((err) => {
+        reject(err);
+        this._unlock();
+      });
+    });
+
+    return new Promise(executor);
+  },
+
+  isLocked: reads('_locked')
+
+});
--- tests/unit/utils/mutex-test.js
+++ tests/unit/utils/mutex-test.js
@@ -1,10 +1,136 @@
-import mutex from 'dummy/utils/mutex';
+import Mutex from 'dummy/utils/mutex';
+import Ember from 'ember';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | mutex');
+const {
+  RSVP: {
+    Promise,
+    all,
+    resolve
+  },
+  run
+} = Ember;
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = mutex();
-  assert.ok(result);
+module('Unit | Utility | mutex', {
+  beforeEach() {
+    this.mutex = Mutex.create();
+  }
+});
+
+test('#lock() returns a fulfilled promise if the given function succeeds', function (assert) {
+  var s = 'abc';
+  var res;
+  run(() => {
+    this.mutex.lock(function () {
+      return s;
+    }).then(function (r) {
+      res = r;
+    });
+  });
+  assert.equal(res, s);
+});
+
+test('#lock() returns a rejected promise if the given function fails', function (assert) {
+  var e = new Error();
+  var err;
+  run(() => {
+    return this.mutex.lock(function () {
+      throw e;
+    }).catch(function (er) {
+      err = er;
+    });
+  });
+  assert.equal(err, e);
+});
+
+test('#lock() returns a rejected promise if the given argument is not a function', function (assert) {
+  var err;
+  run(() => {
+    this.mutex.lock(3).catch(function (e) {
+       err = e;
+    });
+  });
+  assert.equal(err.message, 'argument not function');
+});
+
+test('#lock() allows only one promise chain to run at a time', function (assert) {
+  let done = assert.async();
+  var xs = [];
+
+  var task = function (x) {
+    xs.push(x);
+    return resolve(x);
+  };
+
+  var chain = function (x) {
+    return task(x).then(function (x) {
+      return task(x);
+    }).then(function (x) {
+      return task(x);
+    });
+  };
+
+  var fun = (x) => {
+    return this.mutex.lock(function () {
+      return chain(x);
+    });
+  };
+
+  return all([
+    fun(5),
+    fun(8),
+    fun(11)
+  ]).then(function (rs) {
+    assert.deepEqual(rs, [5, 8, 11]);
+    assert.deepEqual(xs, [5, 5, 5, 8, 8, 8, 11, 11, 11]);
+    done();
+  });
+});
+
+test('#isLocked is false while being not locked', function (assert) {
+  assert.notOk(this.mutex.get('isLocked'));
+});
+
+test('#isLocked is true while being locked', function (assert) {
+  let mutex = this.mutex;
+
+  var task = function () {
+    assert.ok(mutex.get('isLocked'));
+    return resolve(null);
+  };
+
+  var chain = function () {
+    return task().then(function () {
+      return task();
+    });
+  };
+
+  return mutex.lock(chain).then(() => {
+    assert.notOk(mutex.get('isLocked'));
+  });
+});
+
+test('#isLocked is true even between execution loops', function (assert) {
+  let mutex = this.mutex;
+
+  var task = function () {
+    assert.ok(mutex.get('isLocked'));
+    return new Promise(function(resolve) {
+      assert.ok(mutex.get('isLocked'));
+      setTimeout(function() {
+        assert.ok(mutex.get('isLocked'));
+      }, 5);
+      setTimeout(resolve, 10);
+    });
+  };
+
+  var chain = function () {
+    return task().then(function () {
+      return task();
+    });
+  };
+
+  return mutex.lock(chain).then(() => {
+    assert.notOk(mutex.get('isLocked'));
+  });
 });

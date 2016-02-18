3c36610e939a098e09232ad072f4819ea072a6ab
Dmitri Akatov <akatov@gmail.com>
2016-02-18 20:51:33 +0000

    $ ember generate util mutex

---
+++ tests/unit/utils/mutex-test.js
@@ -0,0 +1,10 @@
+import mutex from 'dummy/utils/mutex';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | mutex');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = mutex();
+  assert.ok(result);
+});
